c838dd570609b764b65cf3995323de45f3174614
Paul Meems <bontepaarden@gmail.com>
2021-12-14 18:09:11 +0100

    Extract the service-registration process to a static local function

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,14 +6,25 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
+// ReSharper disable once NotAccessedVariable
 // ReSharper disable once RedundantAssignment
-var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+var baseAddress = builder.HostEnvironment.BaseAddress;
 
 #if DEBUG
-    // Set the correct port when running locally:
-    baseAddress = new Uri("http://localhost:7071");
+// Set the correct port when running locally:
+// ReSharper disable once RedundantAssignment
+baseAddress = "http://localhost:7071";
 #endif
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
+ConfigureServices(builder.Services, baseAddress);
 
 await builder.Build().RunAsync();
+
+// https://github.com/jsakamoto/BlazorWasmPreRendering.Build
+// Extract the service-registration process to the static local function.
+static void ConfigureServices(IServiceCollection services, string baseAddress)
+{
+    services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(baseAddress) });
+
+    // services.AddScoped<IFoo, MyFoo>();
+}
