2acf86b002dcf7e0650673d9c089f8e7ec347456
Paul Meems <bontepaarden@gmail.com>
2021-12-14 13:47:39 +0100

    Use the correct port for the Function when running locally

--- Client/Program.cs
+++ Client/Program.cs
@@ -6,6 +6,14 @@ var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+// ReSharper disable once RedundantAssignment
+var baseAddress = new Uri(builder.HostEnvironment.BaseAddress);
+
+#if DEBUG
+    // Set the correct port when running locally:
+    baseAddress = new Uri("http://localhost:7071");
+#endif
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = baseAddress });
 
 await builder.Build().RunAsync();
