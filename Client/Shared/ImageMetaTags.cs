c3d72ef947cd82eb736e081826b911941ba0e1b3
Paul Meems <bontepaarden@gmail.com>
2021-12-23 14:40:00 +0100

    Updating social meta tags

---
+++ Client/Shared/ImageMetaTags.cs
@@ -0,0 +1,6 @@
+﻿namespace BlazorApp.Client.Shared
+{
+#pragma warning disable SA1313 // Parameter names should begin with lower-case letter
+    public readonly record struct ImageMetaTag(string Name, int Width, int Heigth, string Alt);
+#pragma warning restore SA1313 // Parameter names should begin with lower-case letter
+}
